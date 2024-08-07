4894fccd850d021721ba9f0fc40af508f02e32ec
Daniel Zapata <daniel-dezz@hotmail.com>
2024-08-07 19:09:53 +0200

    controles de flujo

--- Estructuras-control/src/App.java
+++ Estructuras-control/src/App.java
@@ -2,7 +2,7 @@ public class App {
   public static void main(String[] args) throws Exception {
 
     // IF - ELSE IF - ELSE
-
+    System.out.println(">>>>> IF <<<<<<");
     int edad = 61;
 
     if (edad > 18 && edad <= 60) {
@@ -15,8 +15,10 @@ public class App {
       System.out.println("No puedes entrar en la disco");
     }
 
+    System.out.println();
     // SWITCH
 
+    System.out.println(">>>>> SWITCH <<<<<<");
     // ¿Qué tipo de bebida te gusta?
     String bebida = "Cafe";
 
@@ -37,6 +39,62 @@ public class App {
       default:
         System.out.println("Disfruta de tu bebida");
         break;
+    }
+    System.out.println();
+
+    // FOR
+    System.out.println(">>>>> FOR <<<<<<");
+    // for(inicialización; condición; actualización)
+
+    int i;
+
+    for (i = 1; i <= 10; i++) {
+
+      System.out.println(i);
+
+    }
+    System.out.println();
+
+    // WHILE
+    System.out.println(">>>>> WHILE <<<<<<");
+
+    int contador = 1;
+    while (contador <= 5) {
+      System.out.println(contador);
+      contador++;
+    }
+    System.out.println();
+
+    // DO WHILE
+    System.out.println(">>>>> DO WHILE <<<<<<");
+
+    int contador2 = 1;
+
+    do{
+      System.out.println("sucede antes de la condición");
+      System.out.println(contador2);
+      //actualización de la variable
+      contador2++;
+    }while(contador2 <= 5);
+
+    System.out.println(contador2);
+    System.out.println();
+
+    // DO WHILE
+    System.out.println(">>>>> CONTROLES DE FLUJO <<<<<<");
+
+    int z;
+    for(z = 1; z <= 10; z++){
+
+      if (z <=2) {
+        // no ejecutamos el resto del código y pasamos a la siguiente iteración
+        continue;
+      }
+      System.out.println(z);
+      if (z== 5){
+        // break hace que termine el bucle 
+        break;
+      }
     }
 
   }
